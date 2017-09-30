0afd2d4f2d35792f5a37ce7d3afef2477c770eed
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2017-09-30 15:17:50 +0600

    Changed form submitting behaviour to trigger API. Added result heading from script.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	function fetchQuery() {
 
-		$("#search").on( "click", function() {
+		$("#search").submit( function() {
 			var searchQuery = document.getElementById('query').value;
 			var wikiAPICall = "https://en.wikipedia.org/w/api.php?format=json&action=opensearch&search="+searchQuery;
 			// console.log(wikiAPICall);
@@ -11,14 +11,14 @@ $(document).ready(function() {
 				type: "GET",
 				dataType: "jsonp",
 				success: function(wikiData) {
+					$(".query-title").text("Search results for '"+searchQuery+"'");
 					for (i=0; i<wikiData[1].length; i++) {
-						$(".r"+i).html("<div><a href=\""
-							+wikiData[3][i]+"\" target=\"_blank\"><p>"
-							+wikiData[1][i]+"</p></a>"
-							+"<p>"+wikiData[2][i]+"</p></div>");
+						$(".r"+i).html("<div class='wow fadeInUp'><a href='"+wikiData[3][i]+"' target='_blank'><p>"
+							+wikiData[1][i]+"</p></a><p>"+wikiData[2][i]+"</p></div>");
 					}
 				}
 			})
+			event.preventDefault();
 		});
 
 	}
