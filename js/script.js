c463991732545a0a227cb6ce51368acb1dc9e865
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2017-10-05 16:37:02 +0600

    Fixed the margin issue on search bar

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ $(document).ready(function() {
 		$("#search").submit( function() {
 			var searchQuery = document.getElementById('query').value;
 			var wikiAPICall = "https://en.wikipedia.org/w/api.php?format=json&action=opensearch&search="+searchQuery;
-			// console.log(wikiAPICall);
 			$.ajax({
 				url: wikiAPICall,
 				type: "GET",
 				dataType: "jsonp",
 				success: function(wikiData) {
 					$(".query-title").text("Search results for '"+searchQuery+"'");
+					$(".search-corner").css("padding-top", "50px");
 					for (i=0; i<wikiData[1].length; i++) {
 						$(".r"+i).html("<div class='wow fadeInUp'><a href='"+wikiData[3][i]+"' target='_blank'><p>"
 							+wikiData[1][i]+"</p></a><p>"+wikiData[2][i]+"</p></div>");
